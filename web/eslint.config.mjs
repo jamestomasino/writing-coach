c43808bd6975850f5aaebc7c872cb16c3c27ad58
James Tomasino <james@tomasino.org>
2026-04-07 12:09:36 +0000

    Make web lint config compatible with ESLint 10 (#81)

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,10 +1,22 @@
-import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import nextPlugin from '@next/eslint-plugin-next'
+import tseslint from 'typescript-eslint'
 
 const config = [
   {
     ignores: ['.next/**', 'node_modules/**', 'test-results/**', 'playwright-report/**'],
   },
-  ...nextCoreWebVitals,
+  {
+    files: ['**/*.{js,jsx,mjs,ts,tsx,mts,cts}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+  },
+  nextPlugin.configs['core-web-vitals'],
   {
     rules: {
       '@next/next/no-img-element': 'off',
